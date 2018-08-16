f9d513091ba11932b3dd3e4e4569aca214e0de5a
Kumar Abnit <kumar.abnit@mindtree.com>
2018-08-16 12:55:06 +0530

    Initial Commit

---
+++ TestCrossProPOD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCrossProPOD
+//
+//  Created by Kumar Abnit on 16/08/18.
+//  Copyright © 2018 Kumar Abnit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCrossProPOD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCrossProPOD
+//
+//  Created by Kumar Abnit on 16/08/18.
+//  Copyright © 2018 Kumar Abnit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
